ffee3a0896b98c9c8386c5c7362e91344b0231ea
Aliagho <ali.bahrami633@yahoo.com>
2020-03-14 21:33:30 +1100

    added click functionality to history buttons

--- assets/bll.js
+++ assets/bll.js
@@ -1,78 +1,94 @@
 var citiesList = [];
 var historyList = $("#history-list");
+var cityName = "";
 
 localStorageInit();
-
 
 $("#search-button").on("click", displayCurrentWeather);
 
 function displayCurrentWeather() {
-    // event.preventDefault();    
+    event.preventDefault();
 
-    var cityName = capitalizeFirstLetter($.trim($("#cityName").val()));
-    var queryURL1 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",VIC,AU&units=metric&appid=4d5d0bdd780271b5d0440321f345fbb3";
-    var currentDate = moment().format("DD-MMM-YYYY");
-    var temprature = "";
-    var humidity = "";
-    var windSpeed = "";
-    var lat = "";
-    var lon = "";
-    var queryURL2 = ""; // uv index
-    var results = $("#results");
-    var fiveForecast = $("#fiveForecast");
+    // checks if the search button or a history button is clicked
+    var historyCity = $(this).attr("data-cityName");
 
-    var div = $("<div>");
-    div.addClass("border-top-bottom col-md-12");
-
-    // checks if the city exist in the array, it will move the city to the first index of the array otherwise will push it
-    if (citiesList.includes(cityName) === false) {
-        citiesList.push(cityName);
+    if (historyCity) {
+        cityName = historyCity;
     }
     else {
-        // moves the cityName to the first index if already exist in the array
-        citiesList.sort(function (x, y) { return x == cityName ? -1 : y == cityName ? 1 : 0; });
+        cityName = capitalizeFirstLetter($.trim($("#cityName").val()));
     }
 
-    localStorage.setItem("citiesList", citiesList);
-    localStorageInit();
+    // city input text validation - no empty input clicked
+    if (cityName !== "") {
+        var queryURL1 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",VIC,AU&units=metric&appid=4d5d0bdd780271b5d0440321f345fbb3";
+        var currentDate = moment().format("DD-MMM-YYYY");
+        var temprature = "";
+        var humidity = "";
+        var windSpeed = "";
+        var lat = "";
+        var lon = "";
+        var queryURL2 = ""; // uv index
+        var results = $("#results");
+        var fiveForecast = $("#fiveForecast");
 
-    // current weather
-    $.ajax({
-        url: queryURL1,
-        method: "GET"
-    }).then(function (response) {
-        temprature = response.list[0].main.temp;
-        humidity = response.list[0].main.humidity;
-        windSpeed = response.list[0].wind.speed;
-        lat = response.city.coord.lat;
-        lon = response.city.coord.lon;
+        var div = $("<div>");
+        div.addClass("border-top-bottom col-md-12");
 
-        div.append("<h4>" + response.city.name + " (" + currentDate + ")" + "</h4>");
-        div.append("<p>" + "Temprature: " + temprature + "<span class='degree'> &#8451;</span>" + "</p>");
-        div.append("<p>" + "Humidity: " + humidity + "%" + "</p>");
-        div.append("<p>" + "Wind Speed: " + windSpeed + " KPH" + "</p>");
+        localStorageInit();
 
-        queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=4d5d0bdd780271b5d0440321f345fbb3&lat=" + lat + "&lon=" + lon;
+        // current weather
+        $.ajax({
+            url: queryURL1,
+            method: "GET"
+        }).then(function (response) {
+            // checks if the city exist in the array, it will move the city to the first index of the array otherwise will push it
+            if (citiesList.includes(cityName) === false && cityName !== "") {
+                if (response) citiesList.push(cityName);
+            }
+            else {
+                // moves the cityName to the first index if already exist in the array
+                citiesList.sort(function (x, y) { return x == cityName ? -1 : y == cityName ? 1 : 0; });
+            }
 
-        displayUV(queryURL2, div);
+            localStorage.setItem("citiesList", citiesList);
 
-        results.html(div);
+            temprature = response.list[0].main.temp;
+            humidity = response.list[0].main.humidity;
+            windSpeed = response.list[0].wind.speed;
+            lat = response.city.coord.lat;
+            lon = response.city.coord.lon;
 
-        // Five day forecast
-        fiveForecast.empty();
-        for (var i = 0; i < response.list.length; i += 8) {
-            var forecastResultDiv = $("<div>");
-            forecastResultDiv.addClass("five-forecast");
+            div.append("<h4>" + response.city.name + " (" + currentDate + ")" + "</h4>");
+            div.append("<p>" + "Temprature: " + temprature + "<span class='degree'> &#8451;</span>" + "</p>");
+            div.append("<p>" + "Humidity: " + humidity + "%" + "</p>");
+            div.append("<p>" + "Wind Speed: " + windSpeed + " KPH" + "</p>");
 
-            var date = "<p>" + response.list[i].dt_txt + "</p>";
-            var temp = "<p>" + "Temprature: " + response.list[i].main.temp + "<span class='degree'> &#8451;</span>" + "</p>";
-            var humidity = "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>";
+            queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=4d5d0bdd780271b5d0440321f345fbb3&lat=" + lat + "&lon=" + lon;
 
-            forecastResultDiv.html(date + temp + humidity);
+            displayUV(queryURL2, div);
 
-            fiveForecast.append(forecastResultDiv);
-        }
-    });
+            results.html(div);
+
+            // Five day forecast
+            fiveForecast.empty();
+            for (var i = 0; i < response.list.length; i += 8) {
+                var forecastResultDiv = $("<div>");
+                forecastResultDiv.addClass("five-forecast");
+
+                var date = "<p>" + (response.list[i].dt_txt).slice(0, 10) + "</p>"; // slice chooses the date part of the string 0-10
+                var temp = "<p>" + "Temprature: " + response.list[i].main.temp + "<span class='degree'> &#8451;</span>" + "</p>";
+                var humidity = "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>";
+
+                forecastResultDiv.html(date + temp + humidity);
+
+                fiveForecast.append(forecastResultDiv);
+            }
+            cityName = "";
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
 }
 
 // retreives UV index
@@ -100,9 +116,12 @@ function localStorageInit() {
     historyList.empty();
     for (var i = 0; i < citiesList.length; i++) {
         var li = $("<li>");
-        li.addClass("list-group-item");
+        li.addClass("list-group-item city-button");
+        li.attr("data-cityName", citiesList[i]);
         li.text(citiesList[i]);
 
         historyList.append(li);
     }
 }
+
+$(document).on("click", ".city-button", displayCurrentWeather);
