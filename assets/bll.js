0d41f0e240278a7cf6c6a806101a441ec5b82dc6
Aliagho <ali.bahrami633@yahoo.com>
2020-03-14 19:50:39 +1100

    added search history

--- assets/bll.js
+++ assets/bll.js
@@ -1,10 +1,15 @@
+var citiesList = [];
+var historyList = $("#history-list");
+
+localStorageInit();
+
+
 $("#search-button").on("click", displayCurrentWeather);
 
 function displayCurrentWeather() {
-    event.preventDefault();
+    // event.preventDefault();    
 
-    var cityName = $.trim($("#cityName").val());
-    // console.log(cityName);
+    var cityName = capitalizeFirstLetter($.trim($("#cityName").val()));
     var queryURL1 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",VIC,AU&units=metric&appid=4d5d0bdd780271b5d0440321f345fbb3";
     var currentDate = moment().format("DD-MMM-YYYY");
     var temprature = "";
@@ -17,9 +22,21 @@ function displayCurrentWeather() {
     var fiveForecast = $("#fiveForecast");
 
     var div = $("<div>");
-    div.addClass("border-top-bottom");
+    div.addClass("border-top-bottom col-md-12");
 
+    // checks if the city exist in the array, it will move the city to the first index of the array otherwise will push it
+    if (citiesList.includes(cityName) === false) {
+        citiesList.push(cityName);
+    }
+    else {
+        // moves the cityName to the first index if already exist in the array
+        citiesList.sort(function (x, y) { return x == cityName ? -1 : y == cityName ? 1 : 0; });
+    }
 
+    localStorage.setItem("citiesList", citiesList);
+    localStorageInit();
+
+    // current weather
     $.ajax({
         url: queryURL1,
         method: "GET"
@@ -30,44 +47,62 @@ function displayCurrentWeather() {
         lat = response.city.coord.lat;
         lon = response.city.coord.lon;
 
-        div.append("<h4>" + "City: " + response.city.name + "</h4>");
+        div.append("<h4>" + response.city.name + " (" + currentDate + ")" + "</h4>");
         div.append("<p>" + "Temprature: " + temprature + "<span class='degree'> &#8451;</span>" + "</p>");
-        div.append("<p>" + "Humidity: " + humidity + "</p>");
-        div.append("<p>" + "Wind Speed: " + windSpeed + "</p>");
+        div.append("<p>" + "Humidity: " + humidity + "%" + "</p>");
+        div.append("<p>" + "Wind Speed: " + windSpeed + " KPH" + "</p>");
 
         queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=4d5d0bdd780271b5d0440321f345fbb3&lat=" + lat + "&lon=" + lon;
 
-        displayUV(queryURL2);
+        displayUV(queryURL2, div);
 
         results.html(div);
 
+        // Five day forecast
+        fiveForecast.empty();
         for (var i = 0; i < response.list.length; i += 8) {
-            console.log(response.list[i]);
             var forecastResultDiv = $("<div>");
             forecastResultDiv.addClass("five-forecast");
 
             var date = "<p>" + response.list[i].dt_txt + "</p>";
-            var temp = "<p>" + "Temprature: " + response.list[i].main.temp + "</p>";
-            var humidity = "<p>" + "Humidity: " + response.list[i].main.humidity + "</p>";
+            var temp = "<p>" + "Temprature: " + response.list[i].main.temp + "<span class='degree'> &#8451;</span>" + "</p>";
+            var humidity = "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>";
 
             forecastResultDiv.html(date + temp + humidity);
+
             fiveForecast.append(forecastResultDiv);
         }
-
-        // response.list.forEach(function (element) {
-        //     console.log(element.dt);
-        // });
-        // console.log(response.list);
     });
-
-    // retreives UV index
-    function displayUV(queryURL2) {
-        $.ajax({
-            url: queryURL2,
-            method: "GET"
-        }).then(function (response) {
-            div.append("<p>" + "UV Index: " + response.value + "</p>");
-        });
-    }
 }
 
+// retreives UV index
+function displayUV(queryURL2, div) {
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+    }).then(function (response) {
+        div.append("<p>" + "UV Index: " + response.value + "</p>");
+    });
+}
+
+// capitalizes the first letter of a string
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase()
+}
+
+function localStorageInit() {
+    // history list initiation
+    if (localStorage.getItem("citiesList")) {
+        citiesList = localStorage.getItem("citiesList").split(",");
+    }
+
+    // adding search history
+    historyList.empty();
+    for (var i = 0; i < citiesList.length; i++) {
+        var li = $("<li>");
+        li.addClass("list-group-item");
+        li.text(citiesList[i]);
+
+        historyList.append(li);
+    }
+}
